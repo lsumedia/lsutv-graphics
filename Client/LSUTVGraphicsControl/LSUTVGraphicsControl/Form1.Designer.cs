0b900506ad43d692bedfbfe3dbf41b4366bc8f6b
Cameron Small <camerontsmall@googlemail.com>
2017-09-10 13:18:58 +0100

    Start on title card and controller
    Project setup

---
+++ Client/LSUTVGraphicsControl/LSUTVGraphicsControl/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace LSUTVGraphicsControl {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.filePanel = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.serverStatus = new System.Windows.Forms.Label();
+            this.dissolveGraphicsBtn = new System.Windows.Forms.Button();
+            this.clearChannelBtn = new System.Windows.Forms.Button();
+            this.programTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.serverConnectionPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serverAddressField = new System.Windows.Forms.TextBox();
+            this.connectServerBtn = new System.Windows.Forms.Button();
+            this.showTitleCardBtn = new System.Windows.Forms.Button();
+            this.showCountdownL3Btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.filePanel.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.serverConnectionPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // filePanel
+            // 
+            this.filePanel.Controls.Add(this.label1);
+            this.filePanel.Controls.Add(this.programTime);
+            this.filePanel.Controls.Add(this.clearChannelBtn);
+            this.filePanel.Controls.Add(this.dissolveGraphicsBtn);
+            this.filePanel.Controls.Add(this.serverStatus);
+            this.filePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.filePanel.Location = new System.Drawing.Point(0, 0);
+            this.filePanel.Name = "filePanel";
+            this.filePanel.Size = new System.Drawing.Size(883, 29);
+            this.filePanel.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(883, 443);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.serverConnectionPanel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(875, 417);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Setup";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(875, 417);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // serverStatus
+            // 
+            this.serverStatus.AutoSize = true;
+            this.serverStatus.Location = new System.Drawing.Point(12, 9);
+            this.serverStatus.Name = "serverStatus";
+            this.serverStatus.Size = new System.Drawing.Size(73, 13);
+            this.serverStatus.TabIndex = 0;
+            this.serverStatus.Text = "Disconnected";
+            this.serverStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dissolveGraphicsBtn
+            // 
+            this.dissolveGraphicsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dissolveGraphicsBtn.Location = new System.Drawing.Point(603, 3);
+            this.dissolveGraphicsBtn.Name = "dissolveGraphicsBtn";
+            this.dissolveGraphicsBtn.Size = new System.Drawing.Size(131, 23);
+            this.dissolveGraphicsBtn.TabIndex = 1;
+            this.dissolveGraphicsBtn.Text = "Dissolve Graphics (F1)";
+            this.dissolveGraphicsBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearChannelBtn
+            // 
+            this.clearChannelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearChannelBtn.Location = new System.Drawing.Point(740, 3);
+            this.clearChannelBtn.Name = "clearChannelBtn";
+            this.clearChannelBtn.Size = new System.Drawing.Size(131, 23);
+            this.clearChannelBtn.TabIndex = 2;
+            this.clearChannelBtn.Text = "Clear Channel (ESC)";
+            this.clearChannelBtn.UseVisualStyleBackColor = true;
+            // 
+            // programTime
+            // 
+            this.programTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.programTime.AutoSize = true;
+            this.programTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.programTime.Location = new System.Drawing.Point(517, 3);
+            this.programTime.Name = "programTime";
+            this.programTime.Size = new System.Drawing.Size(80, 24);
+            this.programTime.TabIndex = 3;
+            this.programTime.Text = "00:00:00";
+            this.programTime.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(420, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PROGRAM TIME";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(142, 15);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // serverConnectionPanel
+            // 
+            this.serverConnectionPanel.Controls.Add(this.connectServerBtn);
+            this.serverConnectionPanel.Controls.Add(this.serverAddressField);
+            this.serverConnectionPanel.Controls.Add(this.label2);
+            this.serverConnectionPanel.Location = new System.Drawing.Point(11, 7);
+            this.serverConnectionPanel.Name = "serverConnectionPanel";
+            this.serverConnectionPanel.Size = new System.Drawing.Size(204, 123);
+            this.serverConnectionPanel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Caspar Server";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.showCountdownL3Btn);
+            this.panel1.Controls.Add(this.showTitleCardBtn);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Location = new System.Drawing.Point(221, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 151);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Program Start Time";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // serverAddressField
+            // 
+            this.serverAddressField.Location = new System.Drawing.Point(15, 34);
+            this.serverAddressField.Name = "serverAddressField";
+            this.serverAddressField.Size = new System.Drawing.Size(171, 20);
+            this.serverAddressField.TabIndex = 1;
+            this.serverAddressField.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // connectServerBtn
+            // 
+            this.connectServerBtn.Location = new System.Drawing.Point(15, 70);
+            this.connectServerBtn.Name = "connectServerBtn";
+            this.connectServerBtn.Size = new System.Drawing.Size(171, 33);
+            this.connectServerBtn.TabIndex = 2;
+            this.connectServerBtn.Text = "Connect";
+            this.connectServerBtn.UseVisualStyleBackColor = true;
+            // 
+            // showTitleCardBtn
+            // 
+            this.showTitleCardBtn.Location = new System.Drawing.Point(22, 93);
+            this.showTitleCardBtn.Name = "showTitleCardBtn";
+            this.showTitleCardBtn.Size = new System.Drawing.Size(155, 44);
+            this.showTitleCardBtn.TabIndex = 2;
+            this.showTitleCardBtn.Text = "Title Card";
+            this.showTitleCardBtn.UseVisualStyleBackColor = true;
+            // 
+            // showCountdownL3Btn
+            // 
+            this.showCountdownL3Btn.Location = new System.Drawing.Point(184, 93);
+            this.showCountdownL3Btn.Name = "showCountdownL3Btn";
+            this.showCountdownL3Btn.Size = new System.Drawing.Size(158, 44);
+            this.showCountdownL3Btn.TabIndex = 3;
+            this.showCountdownL3Btn.Text = "Countdown L3";
+            this.showCountdownL3Btn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Program Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 472);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.filePanel);
+            this.Name = "Form1";
+            this.Text = "LSUTV Graphics Control";
+            this.filePanel.ResumeLayout(false);
+            this.filePanel.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.serverConnectionPanel.ResumeLayout(false);
+            this.serverConnectionPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel filePanel;
+        private System.Windows.Forms.Label serverStatus;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button clearChannelBtn;
+        private System.Windows.Forms.Button dissolveGraphicsBtn;
+        private System.Windows.Forms.Label programTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel serverConnectionPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverAddressField;
+        private System.Windows.Forms.Button connectServerBtn;
+        private System.Windows.Forms.Button showCountdownL3Btn;
+        private System.Windows.Forms.Button showTitleCardBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
